1392e59d5bd537b97acd8d3ea7ee98c9941126e1
Hex <hex.raeder@gmail.com>
2022-03-10 16:03:33 +0300

    Added Bootstrap and Thymeleaf fragments (navigation bar etc.)

--- src/main/java/com/hex/AutoServiceAppV1/config/WebSecurityConfig.java
+++ src/main/java/com/hex/AutoServiceAppV1/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -36,7 +37,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .rememberMe()
                 .and()
                     .logout()
-                    .permitAll();
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/");
     }
 
     @Override
