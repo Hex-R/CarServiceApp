29b87ed0e4918bc0b4a027b476fa6ed53daa15f8
Hex <hex.raeder@gmail.com>
2022-03-10 16:15:40 +0300

    Some views refactoring

--- src/main/java/com/hex/AutoServiceAppV1/config/WebSecurityConfig.java
+++ src/main/java/com/hex/AutoServiceAppV1/config/WebSecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -37,8 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .rememberMe()
                 .and()
                     .logout()
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                    .logoutSuccessUrl("/");
+                    .permitAll();
     }
 
     @Override
